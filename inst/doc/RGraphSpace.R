5a6f4d05e9458334314ce3f0f38f173f4a769577
Mauro Castro <mauro.a.castro@gmail.com>
2025-05-14 13:50:02 +0000

    version 1.0.8

--- inst/doc/RGraphSpace.R
+++ inst/doc/RGraphSpace.R
@@ -21,8 +21,8 @@ ecount(gtoy1)
 
 # Assign 'x' and 'y' coordinates to each vertex;
 # ..this can be an arbitrary unit in (-Inf, +Inf)
-V(gtoy1)$x <- c(0, 3, -6, -6, -9)
-V(gtoy1)$y <- c(0, 0,  6, -6,  0)
+V(gtoy1)$x <- c(0, 2, -4, -4, -9)
+V(gtoy1)$y <- c(0, 0,  4, -4,  0)
 
 # Assign a name to each vertex
 V(gtoy1)$name <- paste0("n", 1:5)
@@ -95,39 +95,39 @@ E(gtoy1)$arrowLength <- 1
 plotGraphSpace(gtoy1, marks = "n1", mark.color = "white")
 
 ## ----A shortcut for RedeR, eval=FALSE, message=FALSE--------------------------
-#  # Load RedeR, a graph package for interactive visualization
-#  ## Note: for this example, please use Bioc >= 3.19
-#  if(!require("BiocManager", quietly = TRUE)){
-#    install.packages("BiocManager")
-#    #BiocManager::install(version = "3.19")
-#  }
-#  if(!require("RedeR", quietly = TRUE)){
-#    BiocManager::install("RedeR")
-#  }
-#  
-#  # Launch the RedeR application
-#  library(RedeR)
-#  startRedeR()
-#  resetRedeR()
-#  
-#  # Send 'gtoy1' to the RedeR interface
-#  addGraphToRedeR(gtoy1, unit="npc")
-#  relaxRedeR()
-#  
-#  # Fetch the graph with a fresh layout
-#  gtoy2 <- getGraphFromRedeR(unit="npc")
-#  
-#  # Check the round trip...
-#  plotGraphSpace(gtoy2, marks = "n1", mark.color = "white")
-#  
-#  ## Note that for the round trip, shapes and line types are
-#  ## only partially compatible between ggplot2 and RedeR.
-#  
-#  # ...alternatively, just update the graph layout
-#  gtoy2 <- updateLayoutFromRedeR(g=gtoy1)
-#  
-#  # ...check the updated layout
-#  plotGraphSpace(gtoy2, marks = "n1", mark.color = "white")
+# # Load RedeR, a graph package for interactive visualization
+# ## Note: for this example, please use Bioc >= 3.19
+# if(!require("BiocManager", quietly = TRUE)){
+#   install.packages("BiocManager")
+#   #BiocManager::install(version = "3.19")
+# }
+# if(!require("RedeR", quietly = TRUE)){
+#   BiocManager::install("RedeR")
+# }
+# 
+# # Launch the RedeR application
+# library(RedeR)
+# startRedeR()
+# resetRedeR()
+# 
+# # Send 'gtoy1' to the RedeR interface
+# addGraphToRedeR(gtoy1, unit="npc")
+# relaxRedeR()
+# 
+# # Fetch the graph with a fresh layout
+# gtoy2 <- getGraphFromRedeR(unit="npc")
+# 
+# # Check the round trip...
+# plotGraphSpace(gtoy2, marks = "n1", mark.color = "white")
+# 
+# ## Note that for the round trip, shapes and line types are
+# ## only partially compatible between ggplot2 and RedeR.
+# 
+# # ...alternatively, just update the graph layout
+# gtoy2 <- updateLayoutFromRedeR(g=gtoy1)
+# 
+# # ...check the updated layout
+# plotGraphSpace(gtoy2, marks = "n1", mark.color = "white")
 
 ## ----label='Session information', eval=TRUE, echo=FALSE-----------------------
 sessionInfo()
